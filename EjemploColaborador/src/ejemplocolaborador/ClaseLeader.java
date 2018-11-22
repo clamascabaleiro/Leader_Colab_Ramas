07d4160105832283451f5ddae83f82d7d0c2918a
clamascabaleiro <clamascabaleiro@nautilus15.danielcastelao.org>
2018-11-22 09:08:26 +0100

    Modifico la claseLeader añadiendo un SystemOut

--- EjemploColaborador/src/ejemplocolaborador/ClaseLeader.java
+++ EjemploColaborador/src/ejemplocolaborador/ClaseLeader.java
@@ -5,5 +5,7 @@ package ejemplocolaborador;
  * @author clamascabaleiro
  */
 public class ClaseLeader {
-
+    public void metodoAdios(){
+        System.out.println("Adios en clase Leader");
+    }
 }
